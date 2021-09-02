6735a163c1ad7905258649cfed3e1b59332e2132
Rafael <rafael.gcaldas01@gmail.com>
2021-09-01 23:50:28 -0300

    Dashboard and Summary components

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Sumary } from "../Summary";
+
+import { Container } from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Sumary />
+    </Container>
+  );
+}
